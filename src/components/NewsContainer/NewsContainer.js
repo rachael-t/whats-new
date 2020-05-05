502b47ead218f8b971a22723906ab2da9c8274ef
Rachael Thomas <rnthomas001@gmail.com>
2020-05-05 13:40:01 -0600

    Create NewsContainer component and have it display local news data

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = ({ selectedNews }) => {
+    const newsArticlesToRender = selectedNews.map(newsArticle => {
+        return (
+            <article>
+                <img src={newsArticle.img} alt=''/>
+                <h3>{newsArticle.headline}</h3>
+                <p>{newsArticle.description}</p>
+                <a href={newsArticle.url}>Link to Article</a>
+            </article>
+        )
+    })
+    return (
+        <section>
+            {newsArticlesToRender}
+        </section>
+    )
+}
 
 export default NewsContainer;
