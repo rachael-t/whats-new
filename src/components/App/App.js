1d8d896e868314bdac7d48dcd3f9af04f3f161a8
Rachael Thomas <rnthomas001@gmail.com>
2020-05-05 14:49:37 -0600

    Import all data files for different news topics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 
