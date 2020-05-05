b955f9c82156a59cc32e93b026fd5986e42e01d7
Rachael Thomas <rnthomas001@gmail.com>
2020-05-05 13:56:38 -0600

    Build out JSX render for NewsArticle cards

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './NewsArticle.css';
 
 const NewsArticle = (props) => {
+    return (
+        <article>
+            <img src={props.image} alt=''/>
+            <h3>{props.headline}</h3>
+            <p>{props.description}</p>
+            <a href={props.link}>Link to Article</a>
+        </article>
+    )
 }
 
 export default NewsArticle;
