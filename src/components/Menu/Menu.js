f60df0399e35c9343adf1187bce5071cd074b51d
Rachael Thomas <rnthomas001@gmail.com>
2020-05-06 14:54:22 -0600

    Change from class component to functional component and add proptypes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
-import './Menu.css'
+import React from 'react';
+import './Menu.css';
+import PropTypes from 'prop-types';
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: '',
-        };
-    };
-
-    handleChange = (e) => {
-        let selectedTopic = e.target.name;
-        this.props.changeSelectedNews(selectedTopic)
-    }
-
-    render() {
-        return(
-            <div className='menu-container'>
-                <h1>What's New?</h1>
-                <nav>
-                    <button onClick={this.handleChange} name='local'>Local</button>
-                    <button onClick={this.handleChange} name='entertainment'>Entertainment</button>
-                    <button onClick={this.handleChange} name='health'>Health</button>
-                    <button onClick={this.handleChange} name='science'>Science</button>
-                    <button onClick={this.handleChange} name='technology'>Technology</button>
-                </nav>
-            </div>
-        )
-    }
+const Menu = (props) => {
+   
+    return(
+        <div className='menu-container'>
+            <h1>What's New?</h1>
+            <nav>
+                <button onClick={props.changeSelectedNews} name='local'>Local</button>
+                <button onClick={props.changeSelectedNews} name='entertainment'>Entertainment</button>
+                <button onClick={props.changeSelectedNews} name='health'>Health</button>
+                <button onClick={props.changeSelectedNews} name='science'>Science</button>
+                <button onClick={props.changeSelectedNews} name='technology'>Technology</button>
+            </nav>
+        </div>
+    )
 }
 
 export default Menu;
+
+Menu.propTypes = {
+    changeSelectedNews: PropTypes.func,
+}
