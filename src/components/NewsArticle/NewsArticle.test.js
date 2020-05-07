69b57763ff773a09554113c1c6a3cdbd4baf9448
Rachael Thomas <rnthomas001@gmail.com>
2020-05-07 13:50:44 -0600

    Write unit tests for NewsArticle rendering

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -5,16 +5,16 @@ import '@testing-library/jest-dom/extend-expect';
 
 describe('NewsArticle', () => {
     it('display correct information in the NewsArticle', () => {
-        const { getByText } = render(
+        const { getByText, getByAltText } = render(
             <NewsArticle 
-                image={'https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg'}
+                image={'image.jpg'}
                 headline={'The Who postpones Denver Concert at the Pepsi Center'}
                 description={'Classic-rock band The Who has postponed its Denver concert.'}
-                link={'https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/'}
+                link={'https://theknow.denverpost.com/'}
             />);
-        expect(getByText('https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg')).toBeInTheDocument();
+        expect(getByAltText('')).toBeInTheDocument();
         expect(getByText('The Who postpones Denver Concert at the Pepsi Center')).toBeInTheDocument();
         expect(getByText('Classic-rock band The Who has postponed its Denver concert.')).toBeInTheDocument();
-        expect(getByText('https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/')).toBeInTheDocument();
+        expect(getByText('Link to Article')).toBeInTheDocument();
     });
 });
