baee5d5c092d1b97cb6b4f4f565902a3079f60b6
Rachael Thomas <rnthomas001@gmail.com>
2020-05-07 18:03:38 -0600

    Write and pass App component tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should display a topic`s articles when a specific topic is clicked', () => {
+    const { getByText } = render(<App />);
+    fireEvent.click(getByText('Health'));
+    expect(getByText('Optimism has a surprising benefit: Better heart health, study finds')).toBeInTheDocument();
+  });
+
+  it('should display filtered news articles for a topic once searched', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    fireEvent.click(getByText('Health'));
+    fireEvent.change(getByPlaceholderText('Search news articles'), {target: {value: 'heart'}});
+    fireEvent.click(getByText('Search'));
+    expect(getByText('Optimism has a surprising benefit: Better heart health, study finds')).toBeInTheDocument();
+  });
+
+  it('should display a message if no articles match the search text', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+    fireEvent.click(getByText('Health'));
+    fireEvent.change(getByPlaceholderText('Search news articles'), {target: {value: 'cat'}});
+    fireEvent.click(getByText('Search'));
+    expect(getByText('No articles match your search.')).toBeInTheDocument();
+  });
+
 });
